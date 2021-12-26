8aa25da0c2b9999fc5cdd5bc6d41041e1cc6bfbe
iremacildi <iremacildi@gmail.com>
2021-12-26 12:07:09 +0300

    T16 / Create Login Page - FE

--- ui/src/Components/FormLogin.js
+++ ui/src/Components/FormLogin.js
@@ -47,7 +47,7 @@ export default function FormLogin() {
                     <LockOutlinedIcon />
                 </Avatar>
                 <Typography component="h1" variant="h5">
-                    Sign in
+                    Log in
                 </Typography>
                 <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
                     <TextField
@@ -81,7 +81,7 @@ export default function FormLogin() {
                         sx={{ mt: 3, mb: 2 }}
                         style={{ backgroundColor: "darkslategray" }}
                     >
-                        Sign In
+                        Log In
                     </Button>
                     <Grid container>
                         <Grid item xs>
