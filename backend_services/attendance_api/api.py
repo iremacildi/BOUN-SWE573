c05d30fa431137a77312f9429c07952755856050
iremacildi <iremacildi@gmail.com>
2022-01-05 20:42:12 +0300

    T28 / Create Service Request - BE

--- backend_services/attendance_api/api.py
+++ backend_services/attendance_api/api.py
@@ -1,8 +1,24 @@
-from flask import make_response
+from flask import request, jsonify, make_response
 from app import app
+from data.service_request_repository import ServiceRequestRepository
+from model.service_request_model import service_request_model
+import json
 
 @app.route("/")
 def hello():
     resp = make_response(f'This is Attendance API. I am alive.', 200)
     resp.set_cookie('attendanceapi', 'attendance api is working.', httponly=True)
     return resp
+
+@app.route('/servicerequest', methods=['PUT'])
+def create_service():
+    #authentication eklenecek 
+    servicerequest = json.loads(request.data)
+    userid = servicerequest["userid"]
+    serviceid = servicerequest["serviceid"]
+
+    servicerequestrepo = ServiceRequestRepository(serviceid, userid, False, False, True)
+    newservicerequest = servicerequestrepo.add()
+    result = service_request_model.dump(newservicerequest)
+
+    return jsonify(result)
