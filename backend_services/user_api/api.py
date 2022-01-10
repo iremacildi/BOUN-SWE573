7cfb4dd4ca783ef2ee616696d201f0683bf489ab
iremacildi <iremacildi@gmail.com>
2022-01-11 01:08:17 +0300

    T75 / Configure CI/CD

--- backend_services/user_api/api.py
+++ backend_services/user_api/api.py
@@ -53,7 +53,8 @@ def login_user():
     token = guard.encode_jwt_token(user)
     result = {'access_token': token}
     resp = make_response(jsonify(result), 200)
-    resp.set_cookie('access_token', guard.encode_jwt_token(user), httponly=True, samesite="None", secure=True)
+    # resp.set_cookie('access_token', guard.encode_jwt_token(user), httponly=True, samesite="None", secure=True)
+    resp.headers.add('Set-Cookie','access_token=' + guard.encode_jwt_token(user) + '; SameSite=None; Secure')
     return resp
 
 @app.route('/logout', methods=['POST'])
