5c77d1e4d6ad8340cbbd8b495ce4923e18b86181
Irem Acildi Demir <iremacildi@gmail.com>
2022-01-10 17:01:49 +0300

    T75 / Configure CI/CD

--- backend_services/user_api/config.py
+++ backend_services/user_api/config.py
@@ -1,8 +1,4 @@
 CONFIG = {
-   'postgresUrl':${{ secrets.POSTGRESURL }},
-   'postgresUser':${{ secrets.POSTGRESUSER }},
-   'postgresPass':${{ secrets.POSTGRESPASS }},
-   'postgresDb':${{ secrets.POSTGRESDB }},
    'secretKey':'topSecret',
    'jwtAccessLifespan':{'hours': 24},
    'jwtRefreshLifespan':{'days': 30}
