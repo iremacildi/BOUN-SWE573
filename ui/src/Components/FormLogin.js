a3b3323e39538d815830513919537521ad296500
iremacildi <iremacildi@gmail.com>
2022-01-11 06:50:06 +0300

    cookie

--- ui/src/Components/FormLogin.js
+++ ui/src/Components/FormLogin.js
@@ -17,7 +17,7 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
 const userapi = axios.create({
-    baseURL: 'http://13.93.153.147',
+    baseURL: 'https://dev-userapi.westus.azurecontainer.io',
     withCredentials: true
 })
 
